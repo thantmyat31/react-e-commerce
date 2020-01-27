c2760e1ce49456b70b40fc50cfa32a65f0eece6a
thantmyat <thantmyat1234@gmail.com>
2020-01-27 11:54:43 +0630

    change the firebase configuration/add & ignore the firebaseConfig.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,10 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
-const config = {
-	apiKey: 'AIzaSyD0dhdgEH_Y-Ipr0fzqbYMa5RN_coeGo_8',
-	authDomain: 'react-e-commerce-db-f9752.firebaseapp.com',
-	databaseURL: 'https://react-e-commerce-db-f9752.firebaseio.com',
-	projectId: 'react-e-commerce-db-f9752',
-	storageBucket: 'react-e-commerce-db-f9752.appspot.com',
-	messagingSenderId: '793480633888',
-	appId: '1:793480633888:web:58d8882cceabe092d5e645',
-	measurementId: 'G-JV1NP4XJY2'
-};
+//YOUR WEB'S APP FIREBASE CONFIGURATION
+import { firebaseConfig } from '../services/firebaseConfig.js';
+
+const config = firebaseConfig;
 
 export const createUserProfileDocument = async (userAuth, additionlData) => {
 	if (!userAuth) return;
