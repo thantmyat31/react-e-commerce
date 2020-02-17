c11d71c6fb4ed935a1ba11adf0b41ba019b548f8
thantmyat <thantmyat1234@gmail.com>
2020-02-17 13:25:36 +0630

    update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const path = require('path');
-const app = express();
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
@@ -13,33 +15,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'build/client')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, 'build/client', 'index.html'));
-	});
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
-app.listen(port, (error) => {
-	if (error) throw new error();
-	console.log('server is running on ' + port);
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {
-	const body = {
-		source: req.body.token.id,
-		amount: req.body.amount,
-		currency: 'usd'
-	};
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
 
-	stripe.charges.create(body, (stripeErr, stripeRes) => {
-		if (stripeErr) {
-			res.status(500).send({ error: stripeErr });
-		} else {
-			res.status(200).send({ success: stripeRes });
-		}
-	});
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
