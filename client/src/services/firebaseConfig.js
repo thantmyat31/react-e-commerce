7cf3e673e3b6c1ad9c2c626d7d4f5f5456843d1b
thantmyat <thantmyat1234@gmail.com>
2020-02-17 12:48:35 +0630

    add firebaseConfig.js file

---
+++ client/src/services/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const config = {
+	apiKey: 'AIzaSyD0dhdgEH_Y-Ipr0fzqbYMa5RN_coeGo_8',
+	authDomain: 'react-e-commerce-db-f9752.firebaseapp.com',
+	databaseURL: 'https://react-e-commerce-db-f9752.firebaseio.com',
+	projectId: 'react-e-commerce-db-f9752',
+	storageBucket: 'react-e-commerce-db-f9752.appspot.com',
+	messagingSenderId: '793480633888',
+	appId: '1:793480633888:web:58d8882cceabe092d5e645',
+	measurementId: 'G-JV1NP4XJY2'
+};
